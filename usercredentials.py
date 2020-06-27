881f49da60b907a5fe99f13fcdeb4ca4185583e6
Alisha <akmunywoki@gmail.com>
2020-06-28 00:07:33 +0300

    add user class

---
+++ usercredentials.py
@@ -0,0 +1,13 @@
+class Usercredentials:
+
+    """docstring for ."""
+
+    def __init__(self,user_name,password):
+        self.username = username
+        self.password = password
+
+        def save_user(self):
+            '''
+            docstring
+            '''
+            User.user_list.append(self)
